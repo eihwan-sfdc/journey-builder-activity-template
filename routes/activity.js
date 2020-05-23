d099c64575190c0688c668ba99afe6f7990a3297
EihwanKim <cloz2me@gmail.com>
2020-05-23 16:21:59 +0900

    .

--- routes/activity.js
+++ routes/activity.js
@@ -64,7 +64,6 @@ exports.edit = function (req, res) {
 exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     console.log( "*********** save called ***********");
-    console.log( req.body );
     logData(req);
     res.send(200, 'Save');
 };
@@ -85,10 +84,10 @@ exports.execute = function (req, res) {
         }
 
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            
+
             // decoded in arguments
             var decodedArgs = decoded.inArguments[0];
-            
+
             res.send(200, 'Execute');
         } else {
             console.error('inArguments invalid.');
@@ -104,7 +103,6 @@ exports.execute = function (req, res) {
 exports.publish = function (req, res) {
     console.log( "*********** publish called ***********");
     // Data from the req and put it in an array accessible to the main app.
-    console.log( req.body );
     logData(req);
     res.send(200, 'Publish');
 };
@@ -115,7 +113,6 @@ exports.publish = function (req, res) {
 exports.validate = function (req, res) {
     console.log( "*********** validate called ***********");
     // Data from the req and put it in an array accessible to the main app.
-    console.log( req.body );
     logData(req);
     res.send(200, 'Validate');
 };
