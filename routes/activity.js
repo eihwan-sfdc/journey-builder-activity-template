ebbf7cc2413dd2ac6e8106ce78a61f2309942c25
EihwanKim <cloz2me@gmail.com>
2020-05-23 16:22:44 +0900

    .

--- routes/activity.js
+++ routes/activity.js
@@ -29,7 +29,8 @@ function logData(req) {
         secure: req.secure,
         originalUrl: req.originalUrl
     });
-    console.log("body: " + util.inspect(req.body));
+    console.log("inspect body: " + util.inspect(req.body));
+    console.log("body: " + req.body);
     console.log("headers: " + req.headers);
     console.log("trailers: " + req.trailers);
     console.log("method: " + req.method);
