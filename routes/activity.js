e27c5bda702ccc908461f7498cde3a43a76c280d
EihwanKim <cloz2me@gmail.com>
2020-05-24 13:49:11 +0900

    .

--- routes/activity.js
+++ routes/activity.js
@@ -9,17 +9,6 @@ var http = require('https');
 
 exports.logExecuteData = [];
 
-function parseJwt (token) {
-    var str = token + "";
-    var base64Url = str.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-};
-
 function logData(req) {
     exports.logExecuteData.push({
         body: req.body,
